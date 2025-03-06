32e3ff2fa803531ac27771f814abbcff6b68b7a4
Tao Chen <taochen@microsoft.com>
2025-03-06 10:39:42 -0800

    Python: Remove custom separator in Bedrock tool call fully qualified name (#10826)
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    Closes #10619
    
    Bedrock updated its rules on tool names. Previously, Bedrock disallowed
    "-" in tool names, which is the default plugin name and function name
    separator in SK. The solution was to use "_" as the separator. However,
    this introduced a bug where the translation between the SK fully
    qualified function names and the Bedrock tool names would fail if the
    function name had an underscore.
    
    Bedrock now allows "-" in tool names, so we can remove the custom logic
    and just use the default.
    
    Gemini also has rules on how tools are named. It doesn't recommend using
    "-". The solution was to use "_" just like Bedrock. To address the bug
    mentioned above, the solution is to use double underscores.
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    1. Remove Bedrock custom fully qualified function names.
    2. Use double underscores for Gemini fully qualified function names.
    
    TODO: Add a guide for SK function/plugin naming conventions.
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- python/semantic_kernel/connectors/ai/bedrock/services/bedrock_chat_completion.py
+++ python/semantic_kernel/connectors/ai/bedrock/services/bedrock_chat_completion.py
@@ -22,7 +22,6 @@ from semantic_kernel.connectors.ai.bedrock.services.model_provider.bedrock_model
 from semantic_kernel.connectors.ai.bedrock.services.model_provider.utils import (
     MESSAGE_CONVERTERS,
     finish_reason_from_bedrock_to_semantic_kernel,
-    format_bedrock_function_name_to_kernel_function_fully_qualified_name,
     remove_none_recursively,
     update_settings_from_function_choice_configuration,
 )
@@ -314,9 +313,7 @@ class BedrockChatCompletion(BedrockBase, ChatCompletionClientBase):
                 items.append(
                     FunctionCallContent(
                         id=content["toolUse"]["toolUseId"],
-                        name=format_bedrock_function_name_to_kernel_function_fully_qualified_name(
-                            content["toolUse"]["name"]
-                        ),
+                        name=content["toolUse"]["name"],
                         arguments=content["toolUse"]["input"],
                     )
                 )
@@ -383,10 +380,7 @@ class BedrockChatCompletion(BedrockBase, ChatCompletionClientBase):
             items.append(
                 FunctionCallContent(
                     id=event["contentBlockStart"]["start"]["toolUse"]["toolUseId"],
-                    name=format_bedrock_function_name_to_kernel_function_fully_qualified_name(
-                        event["contentBlockStart"]["start"]["toolUse"]["name"]
-                    ),
-                    index=event["contentBlockStart"]["contentBlockIndex"],
+                    name=event["contentBlockStart"]["start"]["toolUse"]["name"],
                 )
             )
 
